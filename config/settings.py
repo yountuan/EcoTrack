9ba6fdd271b5f0f29b221f23ad6e1fdafd85d57b
yountuan <yountuan@Aidas-MacBook-Air.local>
2024-06-01 18:36:19 +0600

    updated allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(' ')
 
 # Application definition
 
